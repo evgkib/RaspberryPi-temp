8d65e77595ababaaa9e014858ae35fcf055a5251
Eugene Kibalko <evgkib@hotmail.com>
2013-08-10 20:50:29 -0500

    essage

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var server = http.createServer(function(req,res){
         res.writeHead(200,{'Content-Type':'text/html'});
         res.end(data, 'utf-8');
     });
-}).listen(3000,"127.0.0.1");
+}).listen(3000);
 
 console.log('Server is running');
 
@@ -16,12 +16,9 @@ var io = require('socket.io').listen(server);
 io.sockets.on('connection', function(socket){
     console.log('User connected');
     setInterval(function(){
-    fs.readFile('./w1-slave', function(error, data){
-        console.log(data.toString().substring(70)/1000);
-
-         //   .indexOf('t='));
-        socket.emit('temperature',{number: data.toString().substring(70)/1000});
-        socket.broadcast.emit('temperature',{number: data.toString().substring(70)/1000});
+    fs.readFile('/sys/bus/w1/devices/28-00000451e0e3/w1_slave', function(error, data){
+        console.log(data.toString().substring(69)/1000);
+        socket.emit('temperature',{number: data.toString().substring(69)/1000});
     });
     }, 1000);
 });
